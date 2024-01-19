91f4a2ac3a667311f1573b76d78525799ee29b90
Casey Miller <cd.miller1230@gmail.com>
2024-01-19 15:36:02 -0700

    removing extras for the moment

--- lua/saguaro/colors.lua
+++ lua/saguaro/colors.lua
@@ -2,8 +2,8 @@ local colors = require("saguaro.palette")
 
 local function select_colors()
   local selected = { none = "none" }
-  selected = vim.tbl_extend("force", selected, colors[vim.g.saguaro.style])
-  selected = vim.tbl_extend("force", selected, vim.g.saguaro.colors)
+  selected = vim.tbl_extend("force", selected, colors[vim.g.saguaro_config.style])
+  selected = vim.tbl_extend("force", selected, vim.g.saguaro_config.colors)
   return selected
 end
 
