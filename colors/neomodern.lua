ae232598eaeac6e63e9490e2497b497527a28b76
Casey Miller <cd.miller1230@gmail.com>
2024-05-17 14:47:01 -0600

    fix: typo

--- colors/neomodern.lua
+++ colors/neomodern.lua
@@ -4,5 +4,5 @@ for k in pairs(package.loaded) do
   end
 end
 
-require("neomodern").setup({ style = "icebreaker" })
+require("neomodern").setup({ style = "iceclimber" })
 require("neomodern").colorscheme()
