0bf6704d46589e3c69537b5e56be84a811d11ade
Casey Miller <cd.miller1230@gmail.com>
2024-05-18 10:01:04 -0600

    fix: lualine ui opts

--- lua/lualine/themes/neomodern.lua
+++ lua/lualine/themes/neomodern.lua
@@ -16,21 +16,21 @@ local colors = {
 -- stylua: ignore
 local neomodern = {
   normal = {
-    a = { bg = cfg.lualine.plain and c.none or colors.norm, fg = cfg.lualine.plain and colors.norm or colors.dark, gui = cfg.lualine.bold and "bold" or "none"},
-    b = { bg = cfg.lualine.plain and c.none or colors.bg_b, fg = colors.vcs },
-    c = { bg = cfg.lualine.plain and c.none or colors.bg_c, fg = colors.fg_dim },
+    a = { bg = cfg.ui.lualine.plain and c.none or colors.norm, fg = cfg.ui.lualine.plain and colors.norm or colors.dark, gui = cfg.ui.lualine.bold and "bold" or "none"},
+    b = { bg = cfg.ui.lualine.plain and c.none or colors.bg_b, fg = colors.vcs },
+    c = { bg = cfg.ui.lualine.plain and c.none or colors.bg_c, fg = colors.fg_dim },
   },
   insert = {
-    a = { bg = cfg.lualine.plain and c.none or colors.ins, fg = cfg.lualine.plain and colors.ins or colors.dark, gui = cfg.lualine.bold and "bold" or "none" },
+    a = { bg = cfg.ui.lualine.plain and c.none or colors.ins, fg = cfg.ui.lualine.plain and colors.ins or colors.dark, gui = cfg.ui.lualine.bold and "bold" or "none" },
   },
   visual = {
-    a = { bg = cfg.lualine.plain and c.none or colors.vis, fg = cfg.lualine.plain and colors.vis or colors.dark, gui = cfg.lualine.bold and "bold" or "none" },
+    a = { bg = cfg.ui.lualine.plain and c.none or colors.vis, fg = cfg.ui.lualine.plain and colors.vis or colors.dark, gui = cfg.ui.lualine.bold and "bold" or "none" },
   },
   replace = {
-    a = { bg = cfg.lualine.plain and c.none or colors.rep, fg = cfg.lualine.plain and colors.rep or colors.dark, gui = cfg.lualine.bold and "bold" or "none" },
+    a = { bg = cfg.ui.lualine.plain and c.none or colors.rep, fg = cfg.ui.lualine.plain and colors.rep or colors.dark, gui = cfg.ui.lualine.bold and "bold" or "none" },
   },
   command = {
-    a = { bg = cfg.lualine.plain and c.none or colors.comm, fg = cfg.lualine.plain and colors.comm or colors.dark, gui = cfg.lualine.bold and "bold" or "none" },
+    a = { bg = cfg.ui.lualine.plain and c.none or colors.comm, fg = cfg.ui.lualine.plain and colors.comm or colors.dark, gui = cfg.ui.lualine.bold and "bold" or "none" },
   },
 }
 
