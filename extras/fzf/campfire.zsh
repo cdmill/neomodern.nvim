dabdbe790bc002f0984edfb0a3ab98e57739fce1
Casey Miller <cd.miller1230@gmail.com>
2024-10-12 18:47:58 -0600

    remove campfire extras

--- extras/fzf/campfire.zsh
+++
@@ -1,5 +0,0 @@
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
---color=fg:#bbbac1,bg:#1b1a20,hl:#d6a56f,gutter:#1b1a20 \
---color=fg+:#bbbac1,bg+:#413f4d,hl+:#d6a56f \
---color=info:#686675,prompt:#8c8abd,pointer:#8c8abd \
---color=marker:#e69875,spinner:#e69875,header:#e69875"
