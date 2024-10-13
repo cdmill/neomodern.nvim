d7994b0f6753915f1a9acd69a298d363bc2b8d3a
Casey Miller <cd.miller1230@gmail.com>
2024-10-12 18:35:54 -0600

    remove campfire

--- lua/neomodern/init.lua
+++ lua/neomodern/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
-M.styles_list =
-  { "iceclimber", "coffeecat", "darkforest", "campfire", "roseprime", "daylight" }
+M.styles_list = { "iceclimber", "coffeecat", "darkforest", "roseprime", "daylight" }
 
 ---Change neomodern option (vim.g.neomodern_config.option)
 ---It can't be changed directly by modifying that field due to a Neovim lua bug with global variables (neomodern_config is a global variable)
