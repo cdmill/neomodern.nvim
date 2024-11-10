d751c831138da9071267c31e6cc28c43904403fd
Casey Miller <cd.miller1230@gmail.com>
2024-11-09 19:09:26 -0700

    fix: fzf highlights

--- lua/neomodern/extras/fzf.lua
+++ lua/neomodern/extras/fzf.lua
@@ -7,9 +7,9 @@ function M.generate(colors)
   return util.template(
     [[
   export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
-  --color=fg:${fg},bg:${bg},hl:${property},gutter:${bg}
-  --color=fg+:${fg},bg+:${line},hl+:${type}
-  --color=info:${comment},prompt:${property},pointer:${property}
+  --color=fg:${fg},bg:${bg},hl:${type},gutter:${bg}
+  --color=fg+:${property},bg+:${line},hl+:${type}
+  --color=info:${comment},prompt:${type},pointer:${property}
   --color=marker:${keyword},spinner:${keyword},header:${keyword}"
   ]],
     colors
