07a484cd71f24ba628bdc91426a29bf7b853d443
cdmill <cd.miller1230@gmail.com>
2025-03-31 21:07:51 -0600

    feat: more fish colors

--- lua/neomodern/extras/fish.lua
+++ lua/neomodern/extras/fish.lua
@@ -12,39 +12,59 @@ function M.generate(colors, url, theme)
         end
     end
 
+    -- TODO: add more global colors
     local fish = util.template(
         [=[
 # name: ${theme}
-## upstream: ${url}
+# upstream: ${url}
 # author: Casey Miller
 
 # use in ~/.config/fish/conf.d/
 
-set -g orange ${orange} # color 16
-set -g alt ${alt} # color 17
+# preferred bg: ${bg}
+# alt background: ${bg}
+
+### full palette. colors defined in neomdern/palette/${theme}.lua
+set -g alt ${alt} 
+set -g constant ${constant} 
+set -g comment ${comment} 
+set -g fg ${fg} 
+set -g func ${func} 
+set -g keyword ${keyword} 
+set -g number ${number} 
+set -g operator ${operator} 
+set -g property ${property} 
+set -g str ${string} 
+set -g type ${type} 
 
 # Syntax Highlighting Colors
 set -g fish_color_normal ${fg}
-set -g fish_color_command ${keyword}
+set -g fish_color_command ${func}
 set -g fish_color_keyword ${keyword}
 set -g fish_color_quote ${string}
-set -g fish_color_redirection ${builtin}
-set -g fish_color_end ${keyword}
+set -g fish_color_redirection ${const}
+set -g fish_color_end ${operator}
 set -g fish_color_error ${diag_red}
 set -g fish_color_param ${fg}
+set -g fish_color_valid_path ${type}
+set -g fish_color_option ${alt}
 set -g fish_color_comment ${comment}
-set -g fish_color_selection --background=${visual}
-set -g fish_color_search_match --background=${visual}
+set -g fish_color_selection --background=${line}
 set -g fish_color_operator ${operator}
-set -g fish_color_escape ${func}
+set -g fish_color_escape ${keyword}
 set -g fish_color_autosuggestion ${comment}
+set -g fish_color_cwd ${func}
+set -g fish_color_hostname ${type}
+set -g fish_color_status ${diag_red}
+set -g fish_color_cancel ${number}
+set -g fish_color_search_match --background=${line}
 
 # Completion Pager Colors}
-set -g fish_pager_color_progress ${comment}
+set -g fish_pager_color_progress ${alt}
 set -g fish_pager_color_prefix ${property}
-set -g fish_pager_color_completion ${fg}
+set -g fish_pager_color_completion ${type}
 set -g fish_pager_color_description ${comment}
-set -g fish_pager_color_selected_background --background=${visual}
+set -g fish_pager_color_selected_background --background=${line}
 ]=],
         fish_colors
     )
--- lua/neomodern/extras/fish_themes.lua
+++ lua/neomodern/extras/fish_themes.lua
@@ -18,28 +18,36 @@ function M.generate(colors, url, theme)
 # upstream: ${url}
 # author: Casey Miller
 
-# preferred_background: ${bg}
+# preferred bg: ${bg}
+# alt background: ${bg}
 
 set -g fish_color_normal ${fg}
-set -g fish_color_command ${keyword}
+set -g fish_color_command ${func}
 set -g fish_color_keyword ${keyword}
 set -g fish_color_quote ${string}
-set -g fish_color_redirection ${builtin}
-set -g fish_color_end ${keyword}
+set -g fish_color_redirection ${const}
+set -g fish_color_end ${operator}
 set -g fish_color_error ${diag_red}
 set -g fish_color_param ${fg}
+set -g fish_color_valid_path ${type}
+set -g fish_color_option ${alt}
 set -g fish_color_comment ${comment}
-set -g fish_color_selection --background=${visual}
-set -g fish_color_search_match --background=${visual}
+set -g fish_color_selection --background=${line}
 set -g fish_color_operator ${operator}
-set -g fish_color_escape ${func}
+set -g fish_color_escape ${keyword}
 set -g fish_color_autosuggestion ${comment}
+set -g fish_color_cwd ${func}
+set -g fish_color_hostname ${type}
+set -g fish_color_status ${diag_red}
+set -g fish_color_cancel ${number}
+set -g fish_color_search_match --background=${line}
 
-set -g fish_pager_color_progress ${comment}
+# Completion Pager Colors}
+set -g fish_pager_color_progress ${alt}
 set -g fish_pager_color_prefix ${property}
-set -g fish_pager_color_completion ${fg}
+set -g fish_pager_color_completion ${type}
 set -g fish_pager_color_description ${comment}
-set -g fish_pager_color_selected_background --background=${visual}
+set -g fish_pager_color_selected_background --background=${line}
 ]=],
         fish_colors
     )
